38b8b83e7e6412eea184b579b78e2ff1a51a7d88
AnkitGurwan <ankitgurwan083@gmail.com>
2023-10-22 01:39:23 +0530

    url updated

--- src/context/auth/AuthState.js
+++ src/context/auth/AuthState.js
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux';
 
 const AuthState = (props) => {
     const [studInfo,setStudInfo] = useState({ name : "" , email : "", roll : ""});
-    const url = "http://localhost:5000";
+    const url = process.env.REACT_APP_FRONTEND_URL;
     const dispatch = useDispatch();
 
     const userLogin = async()=>{
