4a598f42dbef1b5e83b39a7182a7e9158c948f62
AnkitGurwan <ankitgurwan083@gmail.com>
2023-11-11 15:26:27 +0530

    backend url changed

--- src/components/user/userCourse.jsx
+++ src/components/user/userCourse.jsx
@@ -32,6 +32,7 @@ const Home = () => {
 
 
     const getItem = async () => {
+        if(foldersName.length === 0)
         await GetDetails();
         
 
--- src/components/user/userExams.jsx
+++ src/components/user/userExams.jsx
@@ -30,6 +30,7 @@ const Home = () => {
 
     var path =  useSelector(state => state.Files.userPath);
     const getItem = async () => {
+        if(foldersName.length === 0)
         await GetDetails();
         
         if(path.length <= 1)
