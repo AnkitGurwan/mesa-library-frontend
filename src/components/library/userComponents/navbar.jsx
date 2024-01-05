06ab73f4125a8e0e8e2f72020a8fa3eaf383c13d
AnkitGurwan <ankitgurwan083@gmail.com>
2024-01-05 21:19:25 +0530

    Update navbar.jsx

--- src/components/library/userComponents/navbar.jsx
+++ src/components/library/userComponents/navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                     <img src= {logo} alt="logo" className="h-12 w-32 object-fit" />
                 </div>
                 <div>
-                    <Link className='px-5 text-lg md:text-xl hover:underline tracking-wide' to={'/feedback'}>Feedback</Link>
+                    <Link className='px-5 text-lg md:text-xl hover:underline tracking-wide' to={'/library/feedback'}>Feedback</Link>
                     <button onClick={logOutHandler} className='tracking-tight hover:underline hover:scale-105 px-2 mr-5 cursor-pointer text-lg md:text-xl'>Log Out</button>
                 </div>
                 
