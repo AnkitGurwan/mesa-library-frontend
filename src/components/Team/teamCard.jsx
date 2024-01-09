5535d735c24fea068244e4c0e47f1a6fc6f1f5bc
AnkitGurwan <ankitgurwan083@gmail.com>
2024-01-10 01:02:47 +0530

    Updated ui

--- src/components/Team/teamCard.jsx
+++ src/components/Team/teamCard.jsx
@@ -8,7 +8,7 @@ const Card = ({name,image,position,background,social}) => {
         <div ref={ref} className={`h-contain z-1 max-w-[300px] w-full md:w-full flex justify-center align-center text-start flex-col ${inView? 'fadeBelowAnimate':''}`} >
             <div className="h-[270px] w-full relative flex flex-row justify-center align-center text-center">
                 <p className="h-[70%] w-full absolute bottom-0 z-0" style={{backgroundColor:`${background}`}}></p>
-                <img src={image} className="inline h-full  absolute bottom-0  z-2 drop-shadow-[10px_0px_15px_rgba(0,0,0,0.6)]" />
+                <img src={image} className="inline h-full object-cover absolute bottom-0  z-2 drop-shadow-[10px_0px_15px_rgba(0,0,0,0.6)]" />
             </div>
             <p className="text-xl lg:text-2xl font-bold drop-shadow-lg tracking-wide text-[#2e2d30] mt-3 ">{name}</p>
             <p className="text-[14px] lg:text-[17px] font-semibold text-gray-400 drop-shadow-lg tracking-wide uppercase">{position}</p>
