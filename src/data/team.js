655f7e13df06e15693dadd9d3850432cdeaba158
ANKIT GURWAN <109846346+AnkitGurwan@users.noreply.github.com>
2024-01-11 15:49:13 +0530

    Update team.js

--- src/data/team.js
+++ src/data/team.js
@@ -17,11 +17,11 @@ const team = [
     },
     {
         id:2,
-        name:"Devesh Kamble",
-        image:"https://imgs.search.brave.com/metSyjKuX3OGsQ5ZQpRt2PHEpJJi2KzSWJsRG58cdY0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5ncGxheS5jb20v/d3AtY29udGVudC91/cGxvYWRzLzE1L01l/bi1CYWNrZ3JvdW5k/LVBORy1JbWFnZS5w/bmc",
+        name:"Ankit Gurwan",
+        image:"https://firebasestorage.googleapis.com/v0/b/mesa-library.appspot.com/o/uploads%2F12345%2FIMG_20240111_153759_adobe_express.png?alt=media&token=1fc93a28-8528-4164-abf3-6c38df7fefbd",
         background: "#007efd",
-        position:"Web Developer",
-        socialMedia: {instagram:'https://www.instagram.com/deveshkamble_/#', linkedin:'https://www.linkedin.com/in/devesh-kamble/', twitter:"./twitter"},
+        position:"Web Head",
+        socialMedia: {instagram:'https://www.instagram.com/ankit_gurwan', linkedin:'https://www.linkedin.com/in/ankit-gurwan-7979aa223?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',mail: 'mailto:a.gurwan@iitg.ac.in' },
     },
     {
         id:3,
@@ -29,7 +29,7 @@ const team = [
         image:"https://imgs.search.brave.com/metSyjKuX3OGsQ5ZQpRt2PHEpJJi2KzSWJsRG58cdY0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5ncGxheS5jb20v/d3AtY29udGVudC91/cGxvYWRzLzE1L01l/bi1CYWNrZ3JvdW5k/LVBORy1JbWFnZS5w/bmc",
         background: "#fdb9a5",
         position:"Web Developer",
-        socialMedia: {instagram:'https://www.instagram.com/deveshkamble_/#', linkedin:'https://www.linkedin.com/in/devesh-kamble/', twitter:"./twitter", facebook: './facebook'},
+        socialMedia: {instagram:'https://www.instagram.com/deveshkamble_/#', linkedin:'https://www.linkedin.com/in/ankit-gurwan-7979aa223?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app', twitter:"./twitter", facebook: './facebook'},
     },
     {
         id:4,
