6eb6b9baa2e6d9c4cb635f19102b9a6277e2ba02
AnkitGurwan <ankitgurwan083@gmail.com>
2024-01-12 22:43:29 +0530

    update data

--- src/data/team.js
+++ src/data/team.js
@@ -10,10 +10,10 @@ const team = [
     {
         id:1,
         name:"Poorva Upman",
-        image:"https://imgs.search.brave.com/metSyjKuX3OGsQ5ZQpRt2PHEpJJi2KzSWJsRG58cdY0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5ncGxheS5jb20v/d3AtY29udGVudC91/cGxvYWRzLzE1L01l/bi1CYWNrZ3JvdW5k/LVBORy1JbWFnZS5w/bmc",
+        image:"https://firebasestorage.googleapis.com/v0/b/mesa-library.appspot.com/o/uploads%2F12345%2F8ad6fd6a-9a1f-4743-9b70-85bf97c1840e.png?alt=media&token=de5027e2-e9e5-4b5f-b9d0-1ce7834ee378",
         background: "#febc01",
         position:"Vice-President",
-        socialMedia: {},
+        socialMedia: {linkedin: 'https://www.linkedin.com/in/poorva-upman?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',mail: 'mailto:poorvaupman1@gmail.com'},
     },
     {
         id:2,
@@ -27,7 +27,7 @@ const team = [
         id:3,
         name:"Jatin Bansal",
         image:"https://firebasestorage.googleapis.com/v0/b/mesa-library.appspot.com/o/uploads%2F12345%2F42f668c2-23e7-4f6c-8802-e5d9e1ab3455.png?alt=media&token=a1a9c5fb-6a52-4f44-b5b0-038d56220edf",
-        background: "#fdb9a5",
+        background: "#BEB068",
         position:"Events Head",
         socialMedia: {instagram:''},
     },
