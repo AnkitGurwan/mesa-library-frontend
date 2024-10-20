21697822d6a72d2e48323e649e03681abaf8b807
Jatin Varma <147076980+jatinweb2004@users.noreply.github.com>
2024-10-20 12:46:23 +0530

    Create webcontributor.js

---
+++ src/data/webcontributor.js
@@ -0,0 +1,27 @@
+const team = [
+    {
+        id:1,
+        name:"Ankit Gurwan",
+        image:"https://firebasestorage.googleapis.com/v0/b/mesa-library.appspot.com/o/uploads%2F12345%2F2bf80a6c-0377-4460-bf17-f4c936896204.png?alt=media&token=a5d07c84-bae3-4fe3-9331-0042fa51e17a",
+        background: "#5ebec4",
+        // position:"Web Head",
+        socialMedia: {instagram:'https://www.instagram.com/ankit_gurwan/', linkedin:'https://www.linkedin.com/in/ankit-gurwan/',mail: 'mailto:a.gurwan@iitg.ac.in' },
+    },
+    {
+        id:2,
+        name:"Jatin Varma",
+        image:"https://firebasestorage.googleapis.com/v0/b/mesa-8924d.appspot.com/o/phototo.png?alt=media&token=098d7411-a0c3-48f1-8b11-518ab50e987f",
+        background: "#bcfd4c",
+        // position:"Web Head",
+        socialMedia: {instagram:'https://www.instagram.com/varmajatin2004/', linkedin:'https://www.linkedin.com/in/varmajatin/',mail: 'mailto:s.varma@iitg.ac.in' },
+    },
+    {
+        id:3,
+        name:"Devesh Kamble",
+        image:"https://firebasestorage.googleapis.com/v0/b/mesa-8924d.appspot.com/o/Devesh-removebg-preview.png?alt=media&token=f716f7f9-12a6-404a-86d4-fc1111cdde78",
+        background: "#BCF2F6",
+        // position:"Web Head",
+        socialMedia: {instagram:'https://www.instagram.com/deveshkamble_/', linkedin:'https://www.linkedin.com/in/devesh-kamble/',mail: 'mailto:s.varma@iitg.ac.in' },
+    }
+]
+export default team;
