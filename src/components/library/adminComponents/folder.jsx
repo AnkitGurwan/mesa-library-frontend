3e1e909c24ed830994fe9092a524bc26ef6579b1
AnkitGurwan <ankitgurwan083@gmail.com>
2025-01-18 00:25:33 +0530

    added extra informations

--- src/components/library/adminComponents/folder.jsx
+++ src/components/library/adminComponents/folder.jsx
@@ -1,45 +1,89 @@
-import React, { useContext, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useContext, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { setPath } from '../../../redux/storage/storageSlice';
 import folder from '../../images/folder.png';
 import AuthContext from '../../../context/auth/AuthContext';
+import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 const Folder = (props) => {
   const dispatch = useDispatch();
-  const [showDelete,setShowDelete] = useState(false);
-  const { removeFolder , GetDetails } = useContext(AuthContext);
+  const [showDelete, setShowDelete] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [confirmationText, setConfirmationText] = useState('');
+  const { removeFolder, GetDetails } = useContext(AuthContext);
   
   const clickHandler = () => {
     dispatch(setPath(props.name));
-  }
+  };
 
   const deleteHandler = async () => {
-    const x = await removeFolder(props.name,props.parent);
-    if(x===201)GetDetails();
-  }
+    if (confirmationText === 'DELETE') {
+      const x = await removeFolder(props.name, props.parent);
+      if (x === 201) GetDetails();
+      setShowConfirm(false); // Close the confirmation input
+    } else {
+      // You can show an alert or message that the input is incorrect
+      toast.error("Please type 'DELETE' to confirm.", {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+    }
+  };
 
   return (
-    <div className='hover:bg-gray-200 rounded-lg relative'
-    // onMouseEnter={()=>{setShowDelete(true)}}
-    // onMouseLeave={()=>setShowDelete(false)}
+    <div
+      className='hover:bg-gray-200 rounded-lg relative'
+      onMouseEnter={() => { setShowDelete(true); }}
+      onMouseLeave={() => setShowDelete(false)}
     >
-      {showDelete
-      ?
-      <div className='text-end absolute right-0 text-sm flex justify-end p-1'>
-        <i onClick={deleteHandler} class="fa-solid fa-trash bg-gray-400 rounded-full w-6 h-6 flex justify-center items-center cursor-pointer"></i>
-      </div>
-      :
-      ""
-      }
-      <Link to={`${props.name}`} onClick={clickHandler} className='py-3 mx-2 rounded-xl overflow-y-hidden w-24 h-24 flex justify-between items-center cursor-pointer  flex-col'>
+      {showDelete && !showConfirm && (
+        <div className='text-end absolute right-0 text-sm flex justify-end p-1'>
+          <i
+            onClick={() => setShowConfirm(true)}  // Show the confirmation input when clicked
+            className="fa-solid fa-trash bg-gray-400 rounded-full w-6 h-6 flex justify-center items-center cursor-pointer"
+          ></i>
+        </div>
+      )}
+
+      {showConfirm && (
+        <div className='absolute top-0 right-0 bg-white p-4 rounded shadow-md'>
+          <div className="flex flex-col items-center">
+            <p className="mb-2">Type <strong>DELETE</strong> to confirm</p>
+            <input
+              type="text"
+              value={confirmationText}
+              onChange={(e) => setConfirmationText(e.target.value)}
+              className="p-2 border rounded mb-2"
+            />
+            <div className="flex gap-2">
+              <button
+                onClick={deleteHandler}
+                className="bg-red-500 text-white px-4 py-2 rounded"
+              >
+                Delete
+              </button>
+              <button
+                onClick={() => setShowConfirm(false)} // Close the confirmation input
+                className="bg-gray-500 text-white px-4 py-2 rounded"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <Link
+        to={`${props.name}`}
+        onClick={clickHandler}
+        className='py-3 mx-2 rounded-xl overflow-y-hidden w-24 h-24 flex justify-between items-center cursor-pointer flex-col'
+      >
         <img src={folder} alt="folder" className='' />
-        {/* <i class="fa-regular fa-folder px-2 text-2xl font-light"></i> */}
         <div className='text-xs px-1 pt- capitalize'>{props.name}</div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Folder
+export default Folder;
